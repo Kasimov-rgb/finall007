349af6da6ea13c465ed68914ef2f870a430faef7
Your Name <you@example.com>
2024-06-08 16:16:11 +0600

    code

--- core/settings.py
+++ core/settings.py
@@ -171,9 +171,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static
-STATIC_DIR = BASE_DIR / 'static'
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_DIR = BASE_DIR / 'static'
+# STATICFILES_DIRS = [STATIC_DIR]
 
 
 MEDIA_URL = 'media/'
